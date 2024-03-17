b6800623560bdc1dc420c60363ba9c335c4d2036
MarinaBarrov <maari-b@live.com.ar>
2024-03-17 18:43:04 -0300

    Se eliminan metodos sin uso de la interface repository

--- src/main/java/com/dh/ClinicMVC/repository/IOdontologoRepository.java
+++ src/main/java/com/dh/ClinicMVC/repository/IOdontologoRepository.java
@@ -4,16 +4,8 @@ import com.dh.ClinicMVC.entity.Odontologo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface IOdontologoRepository extends JpaRepository<Odontologo, Long> {
-    Odontologo guardar (Odontologo odontologo);
 
-    List<Odontologo> listarTodos();
 
-    Optional<Odontologo> buscarPorId(Long id);
-    void eliminar(Long id);
-    void actualizar(Odontologo odontologo);
 }
